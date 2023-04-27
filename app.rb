6662e01f49f5d26af93fcb40024dff84e3785818
melker-ostling-nit-johaneberg <melker.ostling@elev.ga.ntig.se>
2023-04-27 22:21:31 +0200

    Slut inlämning
    Fixat Yardoc

--- app.rb
+++ app.rb
@@ -7,6 +7,7 @@ require 'sinatra/flash'
 require_relative './model.rb'
 enable :sessions
 
+include Model
 
 
 before do
@@ -18,21 +19,42 @@ before do
 end
 
 
+# Redirects to landing page '/turtle/'
+#
 get('/') do
     redirect('/turtle/')
 end
 
+# Displays landing page with all turtles
+#
+# @see Model#Select_all_features_user
+# @see Model#Select_all_Tags_Descriptions()
 get('/turtle/') do
     result = Select_all_features_user()
     @Tag = Select_all_Tags_Descriptions()
     slim(:"turtle/index", locals:{turtels:result})
 end
 
+# Displays a form to creat a new turtle
+#
+# @see Model#Select_all_Tags()
 get('/turtle/new') do
     tags = Select_all_Tags()
     slim(:"turtle/new", locals:{tags:tags})
 end
 
+# Creates a new turtle and checks some of the input, then it redirects to '/turtle/'
+#
+# @param [String] turtle_name, The turtles name
+# @param [Integer] turtle_size, The size of the turtle
+# @param [String] turtle_species, The species of the turtle
+# @param [Integer] turtle_weight, The weight of the turtle
+# @param [String] turtle_notes, Any specal feature of the turtle
+# @param [Hash] turtle_tags, Wich tags are atrributed to the turtle
+#
+# @see Model#Insert_new_Turtle
+# @see Model#Select_turtle_id
+# @see Model#Insert_turtle_tags
 post('/turtle') do
     if params[:turtle_name].length == 0
         flash[:error] = "Skölpaddan har väll ett namn i alla fall"
@@ -48,6 +70,14 @@ post('/turtle') do
     redirect('/turtle/')
 end
 
+# Displays a form to edit your own turtle
+#
+# @param [Integer] description_id, Which turtle that is edited
+#
+# @see Model#Select_all_features_id
+# @see Model#Check_admin
+# @see Model#Select_all_Tags
+# @see Model#Select_tag_in_use
 get('/turtle/:description_id/edit') do
     id = params[:description_id].to_i
     result = Select_all_features_id(id)
@@ -60,6 +90,19 @@ get('/turtle/:description_id/edit') do
     slim(:"turtle/edit", locals:{user_content:result, tags:tag, tag_in_use:tag_in_use})
 end
 
+# Updates an existing turtle and redirects to '/turtle/'
+#
+# @param [Integer] description_id, Which turtle that is edited
+# @param [String] turtle_name, The turtles name
+# @param [Integer] turtle_size, The size of the turtle
+# @param [String] turtle_species, The species of the turtle
+# @param [Integer] turtle_weight, The weight of the turtle
+# @param [String] turtle_notes, Any specal feature of the turtle
+# @param [Hash] turtle_tags, Which tags are atrributed to the turtle
+#
+# @see Model#Delete_x_description_id
+# @see Model#Update_features
+# @see Model#Insert_turtle_tags
 post('/turtle/:description_id/update') do
     id = params[:description_id].to_i
     turtle_name = params[:turtle_name]
@@ -74,6 +117,13 @@ post('/turtle/:description_id/update') do
     redirect('/turtle/')
 end
 
+# Deletes an existing turtle and checks if it is the correct user ant then redirects to '/turtle/'
+#
+# @param [Integer] description_id, Which turtle that is deleted
+#
+# @see Model#Select_all_features_id
+# @see Model#Check_admin
+# @see Model#Delete_x_description_id
 post('/turtle/:description_id/delete') do
     id = params[:description_id].to_i
     result = Select_all_features_id(id)
@@ -85,10 +135,21 @@ post('/turtle/:description_id/delete') do
     Delete_x_description_id("Rel_Description", id)
     redirect("/turtle/")
 end
+
+# Displays a register form
+#
 get('/register') do
     slim(:register)
 end
 
+# Attempts register and updates the session
+#
+# @param [String] username, The username
+# @param [String] password, The password
+#
+# @see Model#Check_user
+# @see Model#Register_user
+# @see Model#Select_user_password_where_name
 post('/register') do
     username = params[:username]
     password = params[:password]
@@ -101,15 +162,24 @@ post('/register') do
     redirect('/turtle/')
 end
 
+# Displays a login form
+#
 get('/login')do
     slim(:login)
 end
 
 login_attempts = {}
+# Attempts login and updates the session and redirects to '/turtle/'
+#
+# @param [String] username, The username
+# @param [String] password, The password
+#
+# @see Model#Select_user_password_where_name
 post('/login') do
     username = params[:username]
     password = params[:password]
     result = Select_user_password_where_name(username)
+    
 
     if login_attempts[request.ip] != nil && (Time.now.to_i - login_attempts[request.ip]) < 60
         flash[:error] = "Du har försökt för många gånger"
@@ -131,15 +201,25 @@ post('/login') do
     end
 end
 
+# Displays landing page with all posts
+#
+# @see Model#Select_all_post_users
 get('/posts/') do
     result = Select_all_post_users()
     slim(:"post/index", locals:{content:result})
 end
 
+# Displays a form to creat a new post
+#
 get('/post/new') do
     slim(:"post/new")
 end
 
+# Creates a new post and checks it's length, then it redirects to '/posts/'
+#
+# @param [String] content, The content of the post
+#
+# @see Model#Insert_post
 post('/post') do
     content = params[:Content]
     if content.length == 0
@@ -150,6 +230,12 @@ post('/post') do
     redirect('/posts/')
 end
 
+# Displays a form to edit an existing post
+#
+# @param [Integer] post_id, Which post that is edited
+#
+# @see Model#Select_all_post_where_id
+# @see Model#Check_admin
 get('/posts/:post_id/edit') do
     id = params[:post_id].to_i
     result = Select_all_post_where_id(id)
@@ -160,6 +246,12 @@ get('/posts/:post_id/edit') do
     slim(:"post/edit", locals:{post:result})
 end
 
+# Updates an existing post and redirects to '/posts/'
+#
+# @param [Integer] post_id, Which post that is edited
+# @param [String] content, The content of the post
+#
+# @see Model#Update_post
 post('/posts/:post_id/update') do
     id = params[:post_id].to_i
     content = params[:Content]
@@ -167,6 +259,13 @@ post('/posts/:post_id/update') do
     redirect("/posts/")
 end
 
+# Deletes an existing post and redirects to '/posts/'
+#
+# @param [Integer] post_id, Which post that is edited
+#
+# @see Model#Select_all_post_where_id
+# @see Model#Check_admin
+# @see Model#Delete_post_where_id
 post('/posts/:post_id/delete') do
     id = params[:post_id].to_i
     result = Select_all_post_where_id(id)
@@ -178,15 +277,21 @@ post('/posts/:post_id/delete') do
     redirect("/posts/")
 end
 
+# Displays all tags and checks if the user is an admin
+#
+# @see Model#Select_all_Tags
+# @see Model#Check_admin
 get('/admin/tags/') do
     if Check_admin(session[:user_id]) == false
         flash[:error] = "Du har inte tillgång till denna funktionaliteten"
         redirect('/turtle/') 
     end
-    result = Select_all_Tags
+    result = Select_all_Tags()
     slim(:"admin/tag/index", locals:{content:result})
 end
 
+# Displays a form to creat a new tag and checks if the user is an admin
+#
 get('/admin/tags/new') do
     if Check_admin(session[:user_id]) == false
         flash[:error] = "Du har inte tillgång till denna funktionaliteten"
@@ -195,12 +300,22 @@ get('/admin/tags/new') do
     slim(:"admin/tag/new")
 end
 
+# Creates a new tag and redirects to '/admin/tags'
+#
+# @param [String] New_tag, The new tag
+#
+# @see Model#Insert_tags
 post('/admin/tags') do
-    content = params[:New_tag]
-    Insert_tags(content)
+    Insert_tags(params[:New_tag])
     redirect('/admin/tags/')
 end
 
+# Displays a form to edit an existing tag and checks if the user is an admin
+#
+# @param [Integer] tag_id, Which tag that is edited
+#
+# @see Model#Check_admin
+# @see Model#Select_all_tags_where_id
 get('/admin/tags/:tag_id/edit') do
     id = params[:tag_id].to_i
     if Check_admin(session[:user_id]) == false
@@ -211,11 +326,22 @@ get('/admin/tags/:tag_id/edit') do
     slim(:"admin/tag/edit", locals:{edit_tag:result})
 end
 
+# Updates an existing tag and redirects to '/admin/tags/'
+#
+# @param [String] new_tag, The updated tag
+# @param [Integer] tag_id, Which tag that is edited
+#
+# @see Model#Update_tags
 post('/admin/tags/:tag_id/update') do
     Update_tags(params[:new_tag], params[:tag_id].to_i)
     redirect('/admin/tags/')
 end
 
+# Deletes an existing tag and checks if the user is an admin, then it redirects to '/admin/tags/'
+#
+# @param [Integer] tag_id, Which tag that is edited
+#
+# @see Model#Delete_x_tag_id
 post('/admin/tags/:tag_id/delete') do
     tag_id = params[:tag_id].to_i
     if Check_admin(session[:user_id]) == false
@@ -224,5 +350,5 @@ post('/admin/tags/:tag_id/delete') do
     end
     Delete_x_tag_id("Describing_tags", tag_id)
     Delete_x_tag_id("Rel_Description", tag_id)
-    redirect("admin/tags/")
+    redirect("/admin/tags/")
 end
--- model.rb
+++ model.rb
@@ -9,72 +9,162 @@ enable :sessions
 
 
 
-helpers do
+module Model
+    # Connects to sqlite database
+    #
+    # @return [] containing the database
     def Connect_to_db(path)
         db = SQLite3::Database.new("#{path}")
         db.results_as_hash = true
         return db
     end
 
+    # Selects all ID:s to the turtles
+    #
+    # @return [Hash]
+    #   * :Description_Id [Integer] The ID of a Describing_feature/turtle
     def Select_turtle_id
         db = Connect_to_db("db/db.db")
         return db.execute("SELECT Description_Id from Describing_features").last
     end
 
+    # Selects all from Rel_descriptions and describing_tags where tag_id is the same
+    #
+    # @return [Hash]
+    #   * :Tag_Id [Integer] The ID of a tag
+    #   * :Rel_Id [Integer] The ID of a relationship
+    #   * :Description_Id [Integer] The ID of a Describing_feature/turtle
+    #   * :Tag [String] The tag 
     def Select_all_Tags_Descriptions
         db = Connect_to_db("db/db.db")
         tag = db.execute("SELECT * FROM Rel_Description INNER JOIN Describing_tags ON Rel_Description.Tag_Id = Describing_tags.Tag_Id")
         return tag
     end
 
+    # Selects all from Describing_features and Users where User_Id is the same
+    #
+    # @return [Hash]
+    #   * :Description_Id [Integer] The ID of a Description/turtle
+    #   * :Turt_name [String] The turtles name
+    #   * :Size [Integer] The size of the turtle
+    #   * :Species [String] The spices of the turtle
+    #   * :Weight [Integer] The weight of the turtle
+    #   * :Special_notes [String] any special message attaced to the turtle
+    #   * :User_Id [Integer] The ID of a user
+    #   * :Name [String] The name of the user
+    #   * :Password [String] The password of the user
+    #   * :Admin [Integer] Either 1 or 0 for if the user if an admin (1 = admin) (0 = not admin)
     def Select_all_features_user
         db = Connect_to_db("db/db.db")
         result = db.execute("SELECT * FROM Describing_features INNER JOIN Users ON Describing_features.User_Id=Users.User_Id")
         return result
     end
 
+    # Selects all from Describing_features for a Description_Id
+    #
+    # @param [Integer] id The id of the choosen description  
+    #
+    # @return [Hash]
+    #   * :Description_Id [Integer] The ID of a Describing_feature/turtle
+    #   * :Turt_name [String] The turtles name
+    #   * :Size [Integer] The size of the turtle
+    #   * :Species [String] The spices of the turtle
+    #   * :Weight [Integer] The weight of the turtle
+    #   * :Special_notes [String] any special message attaced to the turtle
+    #   * :User_Id [Integer] The ID of a user
     def Select_all_features_id(id)
         db = Connect_to_db("db/db.db")
         result = db.execute("SELECT * from Describing_features WHERE Description_Id=?", id).last
         return result
     end
 
+    # Selects the Tag_Id from Rel_descriptions for a Description_Id
+    #
+    # @param [Integer] id The id of the choosen description  
+    #
+    # @return [Hash]
+    #   * :Tag_Id [Integer] The ID of a tag
     def Select_tag_in_use(id)
         db = Connect_to_db("db/db.db")
         result = db.execute("SELECT Tag_Id FROM Rel_Description WHERE Description_Id=?", id).to_a.flat_map(&:values)
         return result
     end
 
+    # Selects User_Id and Password from User for Name
+    #
+    # @param [String] username The name of the choosen user
+    #
+    # @return [Hash]
+    #   * :User_Id [Integer] The ID of a user
+    #   * :Password [String] The password of the user
     def Select_user_password_where_name(username)
         db = Connect_to_db("db/db.db")
         result = db.execute("SELECT User_Id, Password from Users WHERE Name=?", username).first
         return result
     end
 
+    # Selects all from Post and Users
+    #
+    # @return [Hash]
+    #   * :Post_Id [Integer] The ID of a post
+    #   * :User_Id [Integer] The ID of a user
+    #   * :Content [String] The content of a post
+    #   * :Name [String] The name of the user
+    #   * :Password [String] The password of the user
+    #   * :Admin [Integer] Either 1 or 0 for if the user if an admin (1 = admin) (0 = not admin)
     def Select_all_post_users
         db = Connect_to_db("db/db.db")
         result = db.execute("SELECT * FROM Post INNER JOIN Users ON Post.User_Id = Users.User_Id")
         return result
     end
 
+    # Selects all from Post for a Post_Id
+    #
+    # @param [Integer] id The id of the choosen post  
+    #
+    # @return [Hash]
+    #   * :Post_Id [Integer] The ID of a post
+    #   * :User_Id [Integer] The ID of a user
+    #   * :Content [String] The content of a post
     def Select_all_post_where_id(id)
         db = Connect_to_db("db/db.db")
         result = db.execute("SELECT * FROM POST WHERE Post_Id = ?", id).last
         return result
     end
 
+    # Selects all from Describing_tags for a Tag_Id
+    #
+    # @param [Integer] id The id of the choosen Describing_tag
+    #
+    # @return [Hash]
+    #   * :Tag_Id [Integer] The ID of a tag
+    #   * :Tag [String] The tag 
     def Select_all_tags_where_id(id)
         db = Connect_to_db("db/db.db")
         result = db.execute("SELECT * FROM Describing_tags WHERE Tag_Id = ?", id).last
         return result
     end
 
+    # Selects all from Describing_tags
+    #
+    # @return [Hash]
+    #   * :Tag_Id [Integer] The ID of a tag
+    #   * :Tag [String] The tag 
     def Select_all_Tags
         db = Connect_to_db("db/db.db")
         tags = db.execute("Select * FROM Describing_tags")
         return tags
     end
 
+    # Selects all from Users for a User_id
+    #
+    # @param [Integer] user_id The id of the choosen user
+    #
+    # @return [Hash]
+    #   * :User_Id [Integer] The ID of a user
+    #   * :Name [String] The name of the user
+    #   * :Password [String] The password of the user
+    #   * :Admin [Integer] Either 1 or 0 for if the user if an admin (1 = admin) (0 = not admin)
     def Select_current_user(user_id)
         db = Connect_to_db("db/db.db")
         return db.execute("SELECT * FROM Users WHERE User_Id=?", user_id)
@@ -83,8 +173,10 @@ helpers do
 
 
 
-
-
+    # Attempts to insert a new row for every selected tag in the Rel_Description table
+    #
+    # @param [Hash] turtle_tags The choosen tags of a turtle
+    # @param [Integer] turtle_id The id of the choosen Describing_feature/turtle
     def Insert_turtle_tags(turtle_tags, turtle_id)
         db = Connect_to_db("db/db.db")
         turtle_tags.each do |turtle_tag|
@@ -92,16 +184,32 @@ helpers do
         end
     end
 
+    # Attempts to insert a new row in the Post table
+    #
+    # @param [String] content The content of the post
+    # @param [Integer] user_id The user_id of the creator
     def Insert_post(content, user_id)
         db = Connect_to_db("db/db.db")
         db.execute("INSERT INTO Post (Content, User_id) VALUES (?, ?)", content, user_id)  
     end
 
+    # Attempts to insert a new row in the Describing_tags table
+    #
+    # @param [String] content The new tag
     def Insert_tags(content)
         db = Connect_to_db("db/db.db")
         db.execute("INSERT INTO Describing_tags (Tag) VALUES (?)", content)
     end
 
+    # Attempts to insert a new row in the Describing_tags table
+    #
+    # @param [String] turtle_name The turtles name
+    # @param [Integer] turtle_size The size of the turtle
+    # @param [String] turtle_species The species of the turtle
+    # @param [Integer] turtle_weight The weight of the turtle
+    # @param [String] turtle_notes Any specal feature of the turtle
+    # @param [Hash] turtle_tags Wich tags are atrributed to the turtle
+    # @param [Integer] user_id The user_id of the creator
     def Insert_new_Turtle(turtle_name, turtle_size, turtle_species, turtle_weight, turtle_notes, user_id)
         db = Connect_to_db("db/db.db")
         db.execute("INSERT INTO Describing_features (Turt_Name, Size, Species, Weight, Special_notes, User_Id) VALUES (?, ?, ?, ?, ?, ?)", turtle_name, turtle_size, turtle_species, turtle_weight, turtle_notes, user_id)
@@ -111,17 +219,27 @@ helpers do
 
 
 
-
+    # Attempts to delete a row from a table for a tag_Id 
+    #
+    # @param [String] tabel The name of which table to delete from
+    # @param [Integer] tag_id The tag_id of the tag that is deleted
     def Delete_x_tag_id(tabel, tag_id)
         db = Connect_to_db("db/db.db")
         db.execute("DELETE FROM #{table} WHERE Tag_Id = ?", tag_id)
     end
 
+    # Attempts to delete a row from a table for a description_Id
+    #
+    # @param [String] tabel The name of which table to delete from
+    # @param [Integer] description_id The description_id of the description that is deleted
     def Delete_x_description_id(tabel, description_id)
         db = Connect_to_db("db/db.db")
         db.execute("DELETE FROM #{tabel} WHERE Description_Id = ?", description_id)
     end
 
+    # Attempts to delete a row from the post table for a Post_Id
+    #
+    # @param [Integer] id The id of the choosen post to delete 
     def Delete_post_where_id(id)
         db = Connect_to_db("db/db.db")
         db.execute("DELETE FROM Post WHERE Post_Id = ?", id)
@@ -130,7 +248,14 @@ helpers do
 
 
 
-
+    # Attempts to update a row in the Describing_features table
+    #
+    # @param [String] turtle_name The turtles name
+    # @param [Integer] turtle_size The size of the turtle
+    # @param [String] turtle_species The species of the turtle
+    # @param [Integer] turtle_weight The weight of the turtle
+    # @param [String] turtle_notes Any specal feature of the turtle
+    # @param [Integer] user_id The user_id of the creator
     def Update_features(turtle_name, turtle_size, turtle_species, turtle_weight, turtle_notes, id)
         db = Connect_to_db("db/db.db")
         db.execute("UPDATE Describing_features SET 
@@ -142,6 +267,10 @@ helpers do
             WHERE Description_Id = ?", turtle_name, turtle_size, turtle_species, turtle_weight, turtle_notes, id)
     end
 
+    # Attempts to update a row in the Post table
+    #
+    # @param [String] content The content of the post
+    # @param [Integer] id The user_id of the creator
     def Update_post(content, id)
         db = Connect_to_db("db/db.db")
         db.execute("UPDATE Post SET 
@@ -149,6 +278,10 @@ helpers do
             WHERE Post_id = ?", content, id)
     end
 
+    # Attempts to update a row in the Tag table
+    #
+    # @param [String] content The updated tag
+    # @param [Integer] id The user_id of the creator
     def Update_tags(content, id)
         db = Connect_to_db("db/db.db")
         db.execute("UPDATE Describing_tags SET Tag = ? WHERE Tag_Id = ?", content, tag_id)
@@ -156,7 +289,11 @@ helpers do
 
 
 
-
+    # Checks if the current user is an admin
+    #
+    # @param [Integer] current_user The user_id of user that is currently logged in
+    #
+    # @return [Boolean] whether the current user is an admin and false if no one is logged in
     def Check_admin(current_user)
         db = Connect_to_db("db/db.db")
         a = db.execute("SELECT Admin FROM Users WHERE User_Id = ?", current_user).last
@@ -167,10 +304,21 @@ helpers do
         end
     end
 
+    # Checks if the enterd passwor is correct
+    #
+    # @param [String] password_digest, The correct password
+    # @param [String] password, The enterd password
+    #
+    # @return [Boolean] whether the passwords are the same
     def Check_password(password_digest, password)
         return BCrypt::Password.new(password_digest) == password
     end
 
+    # Checks if there is a user by the same username already
+    #
+    # @param [String] username, The username
+    #
+    # @return [Boolean] true if there are no user by that username else false
     def Check_user(username)
         db = Connect_to_db("db/db.db")
         result = db.execute("SELECT Name FROM Users WHERE Name = ?", username)
@@ -178,7 +326,10 @@ helpers do
     end
 
 
-
+    # Attempts to create a new user
+    #
+    # @param [String] username The name of the new user
+    # @param [String] password The password of the new user
     def Register_user(username, password)
         db = Connect_to_db("db/db.db")
         password_digest = BCrypt::Password.create(password)
